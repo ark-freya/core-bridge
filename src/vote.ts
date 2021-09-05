01f187371472028425cd3da85faa6713012acf8d
alessiodf <35549818+alessiodf@users.noreply.github.com>
2021-09-05 22:30:00 +0000

    fix: call original method and recalculate balances
    It is necessary to call the original updateVoteBalances method in
    order to calculate the difference in vote weight between that and
    our method, which uses an otherwise stock Core 3.0 implementation.
    This is to accommodate chains with custom logic, such as staking,
    by ensuring the staked weight (or any other custom value) is still
    applied to the votes, or else we cannot maintain sync since our
    votes will not match with the rest of the network.

--- src/vote.ts
+++ src/vote.ts
@@ -32,6 +32,7 @@ export class Vote {
         const database: Database.IDatabaseService = app.resolvePlugin<Database.IDatabaseService>("database");
 
         if (database) {
+            (database.walletManager as any)._updateVoteBalances = (database.walletManager as any).updateVoteBalances;
             (database.walletManager as any).updateVoteBalances = function (
                 sender: State.IWallet,
                 recipient: State.IWallet,
@@ -41,76 +42,41 @@ export class Vote {
                 revert: boolean = false
             ): void {
                 if (transaction.type === Enums.TransactionType.Vote && transaction.typeGroup === Enums.TransactionTypeGroup.Core) {
+                    const firstVote: string = transaction.asset.votes[0];
+                    const firstDelegate: State.IWallet = this.findByPublicKey(firstVote.substr(1));
+                    const firstVoteBalance: Utils.BigNumber = firstDelegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
+
+                    this._updateVoteBalances(sender, recipient, transaction, lockWallet, lockTransaction, revert);
+
+                    const firstVoteBalanceAfterUpdate: Utils.BigNumber = firstDelegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
+                    firstDelegate.setAttribute("delegate.voteBalance", firstVoteBalance);
+
                     const senderDelegatedAmount = sender
                         .getAttribute("htlc.lockedBalance", Utils.BigNumber.ZERO)
                         .plus(sender.balance)
                         .minus(revert ? transaction.fee : Utils.BigNumber.ZERO);
+
+                    let firstVoteDifference: Utils.BigNumber;
+
                     for (let i = 0; i < transaction.asset.votes.length; i++) {
                         const vote: string = transaction.asset.votes[i];
                         const delegate: State.IWallet = this.findByPublicKey(vote.substr(1));
 
                         const senderVoteDelegatedAmount = i === 0 && vote.startsWith("-") ? senderDelegatedAmount.plus(transaction.fee) : senderDelegatedAmount;
                         const voteBalanceChange: Utils.BigNumber = senderVoteDelegatedAmount.times(vote.startsWith("-") ? -1 : 1).times(revert ? -1 : 1);
-                        const voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO).plus(voteBalanceChange);
+                        let voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO).plus(voteBalanceChange);
+
+                        if (i === 0) {
+                            firstVoteDifference = voteBalance.minus(firstVoteBalanceAfterUpdate);
+                            voteBalance = voteBalance.minus(firstVoteDifference);
+                        } else {
+                            voteBalance = voteBalance.plus(firstVoteDifference);
+                        }
 
                         delegate.setAttribute("delegate.voteBalance", voteBalance);
                     }
                 } else {
-                    if (sender.hasVoted()) {
-                        const delegate: State.IWallet = this.findByPublicKey(sender.getAttribute("vote"));
-
-                        let amount = transaction.amount;
-                        if (transaction.type === Enums.TransactionType.MultiPayment && transaction.typeGroup === Enums.TransactionTypeGroup.Core) {
-                            amount = transaction.asset.payments.reduce((prev, curr) => prev.plus(curr.amount), Utils.BigNumber.ZERO);
-                        }
-
-                        const total: Utils.BigNumber = amount.plus(transaction.fee);
-
-                        const voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
-                        let newVoteBalance: Utils.BigNumber;
-
-                        if (transaction.type === Enums.TransactionType.HtlcLock && transaction.typeGroup === Enums.TransactionTypeGroup.Core) {
-                            newVoteBalance = revert ? voteBalance.plus(transaction.fee) : voteBalance.minus(transaction.fee);
-                        } else if (transaction.type === Enums.TransactionType.HtlcClaim && transaction.typeGroup === Enums.TransactionTypeGroup.Core) {
-                            newVoteBalance = revert
-                                ? voteBalance.plus(transaction.fee).minus(lockTransaction.amount)
-                                : voteBalance.minus(transaction.fee).plus(lockTransaction.amount);
-                        } else {
-                            newVoteBalance = revert ? voteBalance.plus(total) : voteBalance.minus(total);
-                        }
-
-                        delegate.setAttribute("delegate.voteBalance", newVoteBalance);
-                    }
-
-                    if (transaction.type === Enums.TransactionType.HtlcClaim && transaction.typeGroup === Enums.TransactionTypeGroup.Core && lockWallet.hasAttribute("vote")) {
-                        const lockWalletDelegate: State.IWallet = this.findByPublicKey(lockWallet.getAttribute("vote"));
-                        const lockWalletDelegateVoteBalance: Utils.BigNumber = lockWalletDelegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
-
-                        lockWalletDelegate.setAttribute(
-                            "delegate.voteBalance",
-                            revert ? lockWalletDelegateVoteBalance.plus(lockTransaction.amount) : lockWalletDelegateVoteBalance.minus(lockTransaction.amount)
-                        );
-                    }
-
-                    if (transaction.type === Enums.TransactionType.MultiPayment && transaction.typeGroup === Enums.TransactionTypeGroup.Core) {
-                        for (const { recipientId, amount } of transaction.asset.payments) {
-                            const recipientWallet: State.IWallet = this.findByAddress(recipientId);
-                            if (recipientWallet.hasVoted()) {
-                                const vote = recipientWallet.getAttribute("vote");
-                                const delegate: State.IWallet = this.findByPublicKey(vote);
-                                const voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
-
-                                delegate.setAttribute("delegate.voteBalance", revert ? voteBalance.minus(amount) : voteBalance.plus(amount));
-                            }
-                        }
-                    }
-
-                    if (recipient && recipient.hasVoted() && (transaction.type !== Enums.TransactionType.HtlcLock || transaction.typeGroup !== Enums.TransactionTypeGroup.Core)) {
-                        const delegate: State.IWallet = this.findByPublicKey(recipient.getAttribute("vote"));
-                        const voteBalance: Utils.BigNumber = delegate.getAttribute("delegate.voteBalance", Utils.BigNumber.ZERO);
-
-                        delegate.setAttribute("delegate.voteBalance", revert ? voteBalance.minus(transaction.amount) : voteBalance.plus(transaction.amount));
-                    }
+                    this._updateVoteBalances(sender, recipient, transaction, lockWallet, lockTransaction, revert);
                 }
             };
         }
